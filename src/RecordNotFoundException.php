a4f63215796bf75c04b101a5d8eaa565f2e838d7
sideshow_bob <bernd.rathmanner@student.tuwien.ac.at>
2016-02-10 20:06:45 +0100

    updates RecordNotFoundException constructor which is now compatible with the Exception constructor

--- src/RecordNotFoundException.php
+++ src/RecordNotFoundException.php
@@ -10,7 +10,7 @@ class RecordNotFoundException extends \Exception
     /**
      * @inheritdoc
      */
-    public function __construct($message, \Exception $previous = null, $code = 0)
+    public function __construct($message = "", \Exception $previous = null, $code = 0)
     {
         parent::__construct($message, $code, $previous);
     }
